f987afc76b4bb028a88c0a74e38148aafd4e432e
Chad Estioco <chadestioco@gmail.com>
2014-12-02 18:29:11 +0800

    Dynamic javascript.

---
+++ pax.js
@@ -0,0 +1,27 @@
+var paxJokes = [
+    {
+        "joke":"Pax: Anong tawag sa buhok na pumuputok<br>Everyone: ...<br>Pax: E di...BANGS!",
+        "contributor": "Chad",
+        "comment": "Sa joke na yan ko nakilala si Pax. Antagal bago niya binitawan yung punchline. Kahit si Jason napa-isip nun."
+     },
+     {
+        "joke":"Umoorder kami sa Ramen Cool...<br>Pax: Miss, pwede bang konti-n lang, baka kasi hindi ko maubos yung rame-n eh<br>Everyone: ...<br>Waitress: Single-sized serving po yung nasa menu namin.",
+        "contributor": "Chad",
+        "comment": "Get's niyo yung joke? At first time may bumara kay Pax."
+     },
+     {
+        "joke":"Umoorder kami sa Ramen Cool. May nagtanong kay Pax, \"Pax, Coke?\"<br>Sagot ni Pax, \"Siyempre gusto ko luto.\"",
+        "contributor": "Chad",
+        "comment": "..."
+     },
+     {
+        "joke": "Dinala ko yung laptop ko sa office para mag install ng <em>BioShock Infinite</em>. Nung nakita ni Pax yung laptop ko sabi niya \"Wow Chad parang nag-Aspire ako ah!\"<br><br>Model ng laptop ko? Acer <em>Aspire</em>.",
+        "contributor": "Chad",
+        "comment": "Pag nakita niyo, may partner joke pa yan, tungkol sa <em>BioShock Infinite</em>."
+     },
+     {
+        "joke": "Dinala ko yung laptop ko sa office para mag install ng <em>BioShock Infinite</em>.<br><br>Pax: Chad anong iniinstall mo?<br>Chad: (gives discs)<br>Pax: Wow! Na shock ako!",
+        "contributor": "Chad",
+        "comment": "Pag nakita niyo, may partner joke pa yan. Tungkol naman dun sa laptop ko mismo."
+     }
+]
